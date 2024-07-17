bd51f71ae84125ba55076073bd70440a9faca1e3
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-07-17 02:17:04 -0600

    chore: Update .gitignore to include .env file

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-import AppleProvider from "next-auth/providers/apple"
 import CredentialsProvider from "next-auth/providers/credentials"
 
 export default NextAuth({
