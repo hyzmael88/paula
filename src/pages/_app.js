13c55f329c6e48dca2d63be14104de638b2316b1
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-07-18 23:36:19 -0600

    chore: Move Providers component to components folder and update import paths

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-import {Providers} from './Providers';
+import {Providers} from '../components/Providers';
 export default function App({ Component, pageProps }) {
   return(
     <Providers>
