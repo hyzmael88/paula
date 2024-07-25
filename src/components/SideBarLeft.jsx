fdbd1e807fda52d404d26cdd3032c8d76781b4e0
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-07-25 00:10:43 -0600

    chore: Update SideBarLeft component to improve UI and functionality

--- src/components/SideBarLeft.jsx
+++ src/components/SideBarLeft.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
+import { signOut } from "next-auth/react";
 function SideBarLeft() {
   return (
     <div className="w-1/3 hidden bg-white h-screen border-r-2  xl:flex flex-col justify-between p-4">
@@ -11,67 +12,92 @@ function SideBarLeft() {
           <ul className="space-y-4">
             <li>
               <Link href="/">
-                <p className="block text-lg font-medium text-black hover:text-purple-600">Inicio</p>
+                <p className="block text-lg font-medium text-black hover:text-purple-600">
+                  Inicio
+                </p>
               </Link>
             </li>
             <li>
               <Link href="/Explorar">
-                <p className="block text-lg font-medium text-black hover:text-purple-600">Explorar</p>
+                <p className="block text-lg font-medium text-black hover:text-purple-600">
+                  Explorar
+                </p>
               </Link>
             </li>
             <li>
               <Link href="/Notificaciones">
-                <p className="block text-lg font-medium text-black hover:text-purple-600">Notificaciones</p>
+                <p className="block text-lg font-medium text-black hover:text-purple-600">
+                  Notificaciones
+                </p>
               </Link>
             </li>
             <li>
               <Link href="/Suscripciones">
-                <p className="block text-lg font-medium text-black hover:text-purple-600">Suscripciones</p>
+                <p className="block text-lg font-medium text-black hover:text-purple-600">
+                  Suscripciones
+                </p>
               </Link>
             </li>
             <li>
               <Link href="/Compras">
-                <p className="block text-lg font-medium text-black hover:text-purple-600">Compras</p>
+                <p className="block text-lg font-medium text-black hover:text-purple-600">
+                  Compras
+                </p>
               </Link>
             </li>
             <li>
               <Link href="/Historial">
-                <p className="block text-lg font-medium text-black hover:text-purple-600">Historial de pagos</p>
+                <p className="block text-lg font-medium text-black hover:text-purple-600">
+                  Historial de pagos
+                </p>
               </Link>
             </li>
             <li>
               <Link href="/Faqs">
-                <p className="block text-lg font-medium text-black hover:text-purple-600">FAQS</p>
+                <p className="block text-lg font-medium text-black hover:text-purple-600">
+                  FAQS
+                </p>
               </Link>
             </li>
             <li>
               <Link href="/Soporte">
-                <p className="block text-lg font-medium text-black hover:text-purple-600">Soporte</p>
+                <p className="block text-lg font-medium text-black hover:text-purple-600">
+                  Soporte
+                </p>
               </Link>
             </li>
             <li>
               <Link href="/Configuracion">
-                <p className="block text-lg font-medium text-black hover:text-purple-600">Configuracion</p>
+                <p className="block text-lg font-medium text-black hover:text-purple-600">
+                  Configuracion
+                </p>
               </Link>
             </li>
             <li>
-              <Link href="/Cerrar-sesion">
-                <p className="block text-lg font-medium text-black hover:text-purple-600">Cerrar sesión</p>
-              </Link>
+              <p
+                className="block text-lg font-medium text-black hover:text-purple-600 cursor-pointer"
+                onClick={() => signOut({ callbackUrl: "/" })}
+              >
+                Cerrar sesión
+              </p>
             </li>
           </ul>
         </nav>
       </div>
       <div className="text-center">
         <Link href="/Politicas">
-          <p className="block text-sm font-medium text-black hover:text-purple-600">Políticas de privacidad</p>
+          <p className="block text-sm font-medium text-black hover:text-purple-600">
+            Políticas de privacidad
+          </p>
         </Link>
         <Link href="/Terminos">
-          <p className="block text-sm font-medium text-black hover:text-purple-600">Términos y condiciones</p>
+          <p className="block text-sm font-medium text-black hover:text-purple-600">
+            Términos y condiciones
+          </p>
         </Link>
       </div>
     </div>
   );
-};
+}
 
 export default SideBarLeft;
