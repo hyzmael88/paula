17bfe10e3651fc6fee99f403830e485debe73a36
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-08-06 00:14:30 -0600

    chore: Update stripe-webhook.js for handling checkout session completed event
    - Add imports for 'micro' and 'next-sanity'
    - Create sanityClient using 'next-sanity' package
    - Handle 'checkout.session.completed' event
    - Add logic to update user's paquetesAdquiridos, compras, and subscribedModels in Sanity
    - Remove commented out code and unused variables

--- src/pages/api/stripe-webhook.js
+++ src/pages/api/stripe-webhook.js
@@ -1,18 +1,24 @@
+import { buffer } from 'micro';
+import { createClient } from 'next-sanity';
+const stripe = require('stripe')(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+const sanityClient = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  useCdn: false,
+  apiVersion: '2021-03-25',
+  token: process.env.SANITY_WRITE_TOKEN,
+});
 export const config = {
   api: {
     bodyParser: false,
   },
 };
-
 const webhookSecret = process.env.NEXT_PUBLIC_STRIPE_WEBHOOK_KEY;
-
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const buf = await buffer(req);
     const sig = req.headers['stripe-signature'];
-
     let event;
-
     try {
       event = stripe.webhooks.constructEvent(buf.toString(), sig, webhookSecret);
     } catch (err) {
@@ -22,67 +28,63 @@ export default async function handler(req, res) {
     
     if (event.type === 'checkout.session.completed') {
       const session = event.data.object;
-
       const email = session.customer_details.email;
       const paqueteId = session.metadata.paqueteId;
       const modeloId = session.metadata.modeloId;
       const publicacionId = session.metadata.publicacionId;
-      const subscriptionId = session.subscription; // Obtener el ID de la suscripción de Stripe
-
+      /* const subscriptionId = session.subscription; // Obtener el ID de la suscripción de Stripe */
 
       try {
         const user = await sanityClient.fetch(`*[_type == "usuario" && email == $email][0]`, { email });
-
         if (user) {
-          if(paqueteId){
-            console.log("entre en paquete");
-            await sanityClient
-              .patch(user._id)
-              .setIfMissing({ paquetesAdquiridos: [] })
-              .insert('after', 'paquetesAdquiridos[-1]', [{
-                _type: 'reference',
-                _ref: paqueteId,
-                key: paqueteId
-              }])
-              .commit({ publish: true });
+         if(paqueteId){
+          console.log("entre en paquete")
+          await sanityClient
+          .patch(user._id)
+          .setIfMissing({ paquetesAdquiridos: [] })
+          .insert('after', 'paquetesAdquiridos[-1]', [{
+            _type: 'reference',
+            _ref: paqueteId,
+            _key: paqueteId
+          }])
+          .commit({ publish: true });
 
-            console.log(`Paquete ${paqueteId} añadido al usuario ${user._id}`);
-          }
-          if(publicacionId){
-            await sanityClient
-              .patch(user._id)
-              .setIfMissing({ compras: [] })
-              .insert('after', 'compras[-1]', [{
-                _type: 'reference',
-                _ref: publicacionId,
-                key: publicacionId
-              }])
-              .commit({ publish: true });
+        console.log(`Paquete ${paqueteId} añadido al usuario ${user._id}`);
+         }
+         if(publicacionId){
+          console.log("entre en publicacionId")
+          await sanityClient
+          .patch(user._id)
+          .setIfMissing({ compras: [] })
+          .insert('after', 'compras[-1]', [{
+            _type: 'reference',
+            _ref: publicacionId,
+            _key: publicacionId
+          }])
+          .commit({ publish: true });
 
-            console.log(`Publicacion ${publicacionId} añadido al usuario ${user._id}`);
-          }
-          
-          if(session.mode == "subscription"){
-            console.log("entre en modelo");
-            await sanityClient
-              .patch(user._id)
-              .setIfMissing({ subscribedModels: [] })
-              .insert('after', 'subscribedModels[-1]', [{
-                _type: 'reference', // Define el tipo del objeto
-                ref: { _type: 'reference', _ref: modeloId },
-                key: modeloId,
-                /* subscriptionId // Añadir el ID de la suscripción de Stripe  */
-              }])
-              .commit({ publish: true });
-            console.log(`Modelo ${modeloId} añadido al usuario ${user._id}`);
-          }
+        console.log(`Publicacion ${publicacionId} añadido al usuario ${user._id}`);
+         }
+         if(session.mode == "subscription"){
+          console.log("entre en modelo")
+          await sanityClient
+          .patch(user._id)
+          .setIfMissing({ subscribedModels: [] })
+          .insert('after', 'subscribedModels[-1]', [{
+            _type: 'reference',
+            _ref: modeloId,
+            _key: modeloId
+          }])
+          .commit({ publish: true });
+          console.log(`Paquete ${paqueteId} añadido al usuario ${user._id}`);
         }
+        
+      }
       } catch (error) {
         console.error('Error al actualizar el documento del usuario en Sanity:', error);
         return res.status(500).json({ error: 'Error al actualizar el documento del usuario en Sanity' });
       }
     }
-
     res.status(200).json({ received: true });
   } else {
     res.setHeader('Allow', 'POST');
