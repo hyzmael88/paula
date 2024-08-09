70b2d8c62c91f5982733afd74771af1be587a334
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-08-09 05:56:06 -0600

    Refactor image loading in Configuracion, Modelo/[modelo]/index, and NavbarMovil components

--- src/pages/api/stripePaquetes.js
+++ src/pages/api/stripePaquetes.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
 
     try {
       const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card', 'oxxo'],
+        payment_method_types: ['card'],
         line_items: [
           {
             price_data: {
